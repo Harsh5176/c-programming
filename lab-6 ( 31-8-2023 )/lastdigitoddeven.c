cb348313ae7efca21c1c562b0e4dfb5d28454828
Harsh vekariya <140818605+Harsh5176@users.noreply.github.com>
2023-08-31 10:48:34 +0530

    Add files via upload

---
+++ lab-6 ( 31-8-2023 )/lastdigitoddeven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("enter your val:");
+	scanf("%d", &a);
+	
+	if(a%2==0){
+		printf("last digit is even ");
+	}
+	else{
+		printf("last digit is odd");
+		
+	}
+}
